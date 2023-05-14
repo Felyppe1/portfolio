22f24fd1876c06d198fe9a158362a48ac9738365
Luiz Felyppe <felyppe.nunes1@gmail.com>
2023-05-14 18:16:46 -0300

    docs: arquivos dos textos do site

---
+++ lang/en-us.js
@@ -0,0 +1,43 @@
+const englishData = {
+    header: {
+        aboutMe: 'About Me',
+        projects: 'Projects',
+        langName: 'Português',
+        contactBtnText: 'Contact',
+        menuText1: 'Navigation',
+        aboutMe2: 'About Me',
+        projects2: 'Projects',
+        menuText5: 'Theme',
+        menuText6: 'Language',
+        langName2: 'Português',   
+    },
+    formContainer: {
+        notWorking: 'Not working yet.',
+        formSubtitle: 'Contact',
+        formText3: 'Interested in working together? Send me an email!',
+        nameLabel: 'Name',
+        subjectLabel: 'Subject',
+        messageLabel: 'Message',
+    },
+    section2: {
+        subtitle1: 'About Me',
+        section2Text2: "My name is Luiz Felyppe and I'm a full-stack programmer. I'm currently doing bachelor in Information Systems at UFF (Fluminense Federal University) and I work at ",
+        section2Text3: "Focused on the web development field, I'm in love with technology and I'm constantly learning in order to improve my programming abilities.",
+        subtitle2: 'Technologies'
+    },
+    section3: {
+        section3Subtitle: 'Projects',
+        project1: {
+            projectDescription: 'A coffee e-commerce proposed as a challange in the Rocketseat course. This project was made to learn concepts of React JS and TypeScript.',
+            sourceCode: 'Source code'
+        },
+        project2: {
+            projectName: 'Calculator',
+            projectDescription: 'A copy of a calculator I have at home with pretty much the same functionalities. At the next version it will be totally complete. Project made to practice JavaScript',
+            sourceCode: 'Source code'
+        },
+        project3: {
+            soon: 'Soon'
+        }
+    }
+}
---
+++ lang/pt-br.js
@@ -0,0 +1,43 @@
+const text = {
+    header: {
+        aboutMe: 'Sobre Mim',
+        projects: 'Projetos',
+        langName: 'English',
+        contactBtnText: 'Contato',
+        menuText1: 'Navegação',
+        aboutMe2: 'Sobre Mim',
+        projects2: 'Projetos',
+        menuText5: 'Tema',
+        menuText6: 'Idioma',
+        langName2: 'English',   
+    },
+    formContainer: {
+        notWorking: 'Não está funcionando ainda.',
+        formSubtitle: 'Contato',
+        formText3: 'Interessado em trabalharmos juntos? Me envie um email!',
+        nameLabel: 'Nome',
+        subjectLabel: 'Assunto',
+        messageLabel: 'Mensagem',
+    },
+    section2: {
+        subtitle1: 'Sobre mim',
+        section2Text2: 'Meu nome é Luiz Felyppe e eu sou programador full-stack. Atualmente, faço Sistemas de Informação na UFF (Universidade Federal Fluminense) e trabalho na',
+        section2Text3: 'Focado na área de desenvolvimento web, sou apaixonado por tecnologia e estou em constante aprendizado visando a melhorar minhas habilidades na programação.',
+        subtitle2: 'Tecnologias'
+    },
+    section3: {
+        section3Subtitle: 'Projetos',
+        project1: {
+            projectDescription: 'Um e-commerce de cafés proposto como desafio no curso da Rocketseat. Esse projeto foi realizado para fixar conceitos de React JS com TypeScript.',
+            sourceCode: 'Código fonte'
+        },
+        project2: {
+            projectName: 'Calculadora',
+            projectDescription: 'A réplica de uma calculadora que tenho em casa com praticamente as mesmas funcionalidades. Na próxima versão estará completa. Projeto realizado para praticar JavaScript.',
+            sourceCode: 'Código fonte'
+        },
+        project3: {
+            soon: 'Em breve'
+        }
+    }
+}
