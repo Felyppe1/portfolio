4fb24da1157cdfdeb8811405d5e0bee571beb828
Luiz Felyppe <felyppe.nunes1@gmail.com>
2023-05-18 10:29:28 -0300

    fix: sticky hover

--- script.js
+++ script.js
@@ -393,7 +393,7 @@ function handleChangeLanguage() {
 let themeDiv2 = document.querySelector(".theme-div2")
 themeDiv2.addEventListener('touchend', () => {
     if (darkMode == true) {
-        themeDiv2.style.backgroundColor = '0e0e0e'
+        themeDiv2.style.backgroundColor = '#0e0e0e'
     }
     else {
         themeDiv2.style.backgroundColor = '#f4f4f4'
